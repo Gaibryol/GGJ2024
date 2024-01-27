1f437366e61174b5cc8a8b7ba8d382860abef335
akchen1 <43446512+akchen1@users.noreply.github.com>
2024-01-26 20:29:17 -0700

    set up base types

---
+++ Assets/GameAssets/System/AnimalCharacteristic.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalCharacteristic : ScriptableObject
+{
+    public List<RecipeItems> recipeItems;
+    public AnimalCharacteristicType characteristicType;
+
+    public Sprite sprite;
+}
---
+++ Assets/GameAssets/System/AnimalCharacteristicType.cs
@@ -0,0 +1,1 @@
+public enum AnimalCharacteristicType { Hat, Shirt, Pants, }
