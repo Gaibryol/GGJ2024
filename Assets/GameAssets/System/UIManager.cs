3cdf20fab99a5703c9651437ebb2abcf3a4fb702
Gaibryol <gaibryol@gmail.com>
2024-01-27 12:58:52 -0700

    Added timer in top left

--- Assets/GameAssets/System/UIManager.cs
+++ Assets/GameAssets/System/UIManager.cs
@@ -9,24 +9,57 @@ public class UIManager : MonoBehaviour
 {
 	private EventBrokerComponent eventBroker = new EventBrokerComponent();
 
-
 	[SerializeField] private TMP_Text timeText;
 
-	private float startTime;
-	private float currentTime;
+	private DateTime currentDateTime;
+	private bool middleOfDay;
 
-    // Start is called before the first frame update
-    void Start()
+	private float timer;
+
+	private void Awake()
+	{
+		middleOfDay = false;
+		timer = 0f;
+	}
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		currentDateTime = Constants.GameSystem.startingDateTime;
+		timeText.text = FormatCurrentTime();
+	}
+
+	// Update is called once per frame
+	void Update()
     {
-        
+		if (middleOfDay)
+		{
+			Debug.Log(timer);
+			timer += Time.deltaTime;
+			if (timer > Constants.GameSystem.SecondsPerHour)
+			{
+				currentDateTime = currentDateTime.AddHours(1);
+				timeText.text = FormatCurrentTime();
+				timer = 0f;
+			}
+		}
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-		currentTime = Time.time - startTime;
-		timeText.text = currentTime.ToString();
-    }
+	private string  FormatCurrentTime()
+	{
+		if (currentDateTime.Hour < 12)
+		{
+			return currentDateTime.ToString("HH:mm") + " AM";
+		}
+		else if (currentDateTime.Hour == 12)
+		{
+			return currentDateTime.ToString("HH:mm") + " PM";
+		}
+		else
+		{
+			return currentDateTime.AddHours(-12).ToString("HH:mm") + " PM";
+		}
+	}
 
 	public void PressSprayButton()
 	{
@@ -35,17 +68,25 @@ public class UIManager : MonoBehaviour
 
 	private void StartDayHandler(BrokerEvent<GameSystemEvents.StartDay> inEvent)
 	{
-		Debug.Log(inEvent.Payload.Day);
-		startTime = inEvent.Payload.StartTime;
+		middleOfDay = true;
+		timer = 0f;
+		currentDateTime = Constants.GameSystem.startingDateTime;
+	}
+
+	private void EndDayHandler(BrokerEvent<GameSystemEvents.EndDay> inEvent)
+	{
+		middleOfDay = false;
 	}
 
 	private void OnEnable()
 	{
 		eventBroker.Subscribe<GameSystemEvents.StartDay>(StartDayHandler);
+		eventBroker.Subscribe<GameSystemEvents.EndDay>(EndDayHandler);
 	}
 
 	private void OnDisable()
 	{
 		eventBroker.Unsubscribe<GameSystemEvents.StartDay>(StartDayHandler);
+		eventBroker.Unsubscribe<GameSystemEvents.EndDay>(EndDayHandler);
 	}
 }
